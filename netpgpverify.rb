05f617b6a6425984313063ecd677dadb271b97f1
Ross Nelson <ross.nelson@gmail.com>
2015-09-13 11:00:19 -0500

    Added netpgpverify [20150204]

---
+++ netpgpverify.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class Netpgp < Formula
+  homepage "http://www.netpgp.com"
+  url "http://www.netpgp.com/src/netpgpverify-20150204.tar.gz"
+  sha1 "19afc8cfaf4e6159b0c2bd64562b45e52ecbd904"
+  version "20150204"
+
+  def install
+    ENV["CFLAGS"] = "-Wno-deprecated-declarations"
+    system "./configure", "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/netpgpverify", "-v"
+  end
+end
